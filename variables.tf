09dd8c312bfd7a430d06897c8b910db7debf017e
Ewere Diagboya <zodiacten@hotmail.com>
2021-05-26 21:04:43 +0100

    removed vpc/subnet static values

--- variables.tf
+++ variables.tf
@@ -1,19 +1,19 @@
 variable "aws_cgws" {
   description = "Create Customer Gateways"
   default     = {
-      "abuja_cgw"   = "1.1.1.1",
-      "ph_cgw"      = "2.2.2.2",
-      "vi_cgw"      = "3.3.3.3",
-      "anthony_cgw" = "4.4.4.4"
+      "one_cgw"   = "1.1.1.1",
+      "two_cgw"      = "2.2.2.2",
+      "three_cgw"      = "3.3.3.3",
+      "four_cgw" = "4.4.4.4"
   }
 }
 
 variable "vpcsubnet" {
   description = "VPC Subnet"
-  default     = ["subnet-06b60af59ada83699"]
+  
 }
 
 variable "vpc" {
   description = "VPC"
-  default     = "vpc-047150b8a082c4422"
+  
 }
